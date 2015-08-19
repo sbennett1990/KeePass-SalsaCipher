400f2be0d1d6b749626fb1dc36230e2344f32d3d
sbennett1990 <sbennett1990@gmail.com>
2015-08-19 18:55:36 -0400

    Update the make-plgx.bat file

--- make-plgx.bat
+++ make-plgx.bat
@@ -1,2 +1,13 @@
 REM This build file is very rigid and highly un-portable. I use is as a convenience on my own computer.
-.\KeePass.exe --plgx-create C:\Users\Scott\Documents\Code\GitHub\KeePass-SalsaCipher\Salsa20Cipher --plgx-prereq-kp:2.18 --plgx-prereq-os:Windows --plgx-prereq-net:4
+REM Note to self: Run this file within the KeePass directory!
+
+SET basefolder=C:\Users\Scott\Documents\Code\GitHub\KeePass-SalsaCipher
+SET srcfolder=%basefolder%\Salsa20Cipher
+SET plgx=%basefolder%\Salsa20Cipher.plgx
+
+rd %srcfolder%\bin /S /Q
+rd %srcfolder%\obj /S /Q
+
+.\KeePass.exe --plgx-create %srcfolder% --plgx-prereq-kp:2.18 --plgx-prereq-os:Windows --plgx-prereq-net:4
+
+xcopy %plgx% /Y

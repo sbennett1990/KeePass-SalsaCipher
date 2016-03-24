0ff43f01085580e7496385f1d79f871c285b3d08
Scott Bennett <scott.bennett@lowes.com>
2016-03-23 20:39:02 -0400

    Update make-plgx.bat
    Remove the REM statements in favor of actual batch comments. Also remove
    the username from the path to the source folder and replace it with
    %username%.

--- make-plgx.bat
+++ make-plgx.bat
@@ -1,7 +1,7 @@
-REM This build file is very rigid and highly un-portable. I use is as a convenience on my own computer.
-REM Note to self: Run this file within the KeePass directory!
+:: This build file is very rigid and highly un-portable. I use is as a convenience on my own computer.
+:: Note to self: Run this file within the KeePass directory!
 
-SET basefolder=C:\Users\Scott\Documents\Code\GitHub\KeePass-SalsaCipher
+SET basefolder=C:\Users\%username%\Documents\Code\GitHub\KeePass-SalsaCipher
 SET srcfolder=%basefolder%\Salsa20Cipher
 SET plgx=%basefolder%\Salsa20Cipher.plgx
 

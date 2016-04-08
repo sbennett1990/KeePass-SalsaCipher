22a068154f3f4809f079c2778128fddf707455a1
Scott Bennett <scottb@fastmail.com>
2016-04-08 16:44:30 -0400

    Add a test case to BitTwiddleTest

--- Tests/BitTwiddleTest.cs
+++ Tests/BitTwiddleTest.cs
@@ -36,13 +36,19 @@ using Salsa20Cipher;
 namespace Tests {
     [TestFixture]
     public class BitTwiddleTest {
+        [Test]
+        public void Add_TwoUints_ReturnsSum() {
+            uint a = 256;
+            uint b = 256;
+            uint expected = 512;
 
-        public void Add_TwoInts_ReturnsSum() {
+            uint sum = bits.Add(a, b);
 
+            Assert.That(sum, Is.EqualTo(expected));
         }
 
 
-        public void Add_TwoBigInts_ReturnsOverflowSum() {
+        public void Add_TwoBigUints_ReturnsOverflowSum() {
 
         }
     }

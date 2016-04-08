5874660462bb9d41b6c2ca56c9616ce4845864e7
Scott Bennett <scottb@fastmail.com>
2016-04-08 16:59:45 -0400

    Update make-plgx.bat file to be better

--- make-plgx.bat
+++ make-plgx.bat
@@ -1,28 +1,25 @@
 @echo off
 goto :TopOfCode
-
 =======================================================================
 make-plgx.bat
 
  Notes:
-    This build file is very rigid and highly un-portable. I use is as a convenience on my own computer.
-    
-    This file should be run within the KeePass installation directory. 
-    
-    Update %basefolder% below to point to your development directory.
-=======================================================================
+    This build file is slightly rigid and a little un-portable. I use it as a convenience
+    on my own computer.
 
+    Update %keepassDir% below to point to your installation of KeePass.
+=======================================================================
 :TopOfCode
 @echo on
-REM Note to self: Run this file within the KeePass installation directory!
 
-set basefolder=C:\Users\%username%\Documents\Code\GitHub\KeePass-SalsaCipher
-set srcfolder=%basefolder%\Salsa20Cipher
-set plgx=%basefolder%\Salsa20Cipher.plgx
+set keepassDir="C:\Program Files (x86)\KeePass"
+set srcfolder=.\Salsa20Cipher
+set plgx=.\Salsa20Cipher.plgx
 
 rd %srcfolder%\bin /S /Q
 rd %srcfolder%\obj /S /Q
 
-.\KeePass.exe --plgx-create %srcfolder% --plgx-prereq-kp:2.18 --plgx-prereq-net:3
+%keepassDir%\KeePass.exe --plgx-create %srcfolder% --plgx-prereq-kp:2.18 --plgx-prereq-net:3
 
-xcopy %plgx% /Y
+:: Copy the plgx into the KeePass installation directory
+xcopy %plgx% %keepassDir% /Y

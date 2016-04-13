302a16d62ddce21f8d28903d23c161fc19a41a01
Scott Bennett <scottb@fastmail.com>
2016-04-13 13:03:18 -0400

    Provide the absolute path the project folder

--- make-plgx.bat
+++ make-plgx.bat
@@ -4,17 +4,21 @@ goto :TopOfCode
 make-plgx.bat
 
  Notes:
-    This build file is slightly rigid and a little un-portable. I use it as a convenience
-    on my own computer.
+    This build file is very rigid and highly un-portable. I use it as a convenience
+    on my own computer. This is because a relative path to the source code project
+    folder will *not* work with the --plgx-create command.
 
-    Update %keepassDir% below to point to your installation of KeePass.
+    For more info, see: http://keepass.info/help/v2_dev/plg_index.html#plgx
+
+    Update %srcfolder% below with your full path to the project folder
+    Update %keepassDir% below to point to your installation of KeePass
 =======================================================================
 :TopOfCode
 @echo on
 
 set keepassDir="C:\Program Files (x86)\KeePass"
-set srcfolder=.\Salsa20Cipher
-set plgx=.\Salsa20Cipher.plgx
+set srcfolder="C:\Users\sbennet\Documents\Code\GitHub\KeePass-SalsaCipher\Salsa20Cipher"
+set plgx=Salsa20Cipher.plgx
 
 rd %srcfolder%\bin /S /Q
 rd %srcfolder%\obj /S /Q
